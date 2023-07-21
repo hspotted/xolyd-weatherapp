1460d85ab57d3f02a3ebaef4475c8328881c7705
Henrique Fernandes <hspotted@gmail.com>
2023-07-21 12:04:23 +0100

    style: configure eslint and prettier
    Configure the errors ocurrer when install the node packages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
+import "@/styles/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import type { ReactNode } from "react";
-import "@/styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
